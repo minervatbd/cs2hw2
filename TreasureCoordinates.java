8a5cfd733cd330f1e48e3511f0a9f1516fdcf246
Minerva <minervatbd>
2024-09-12 11:10:47 -0400

    determine coordinates function and input handling

--- TreasureCoordinates.java
+++ TreasureCoordinates.java
@@ -5,7 +5,24 @@
  */
 
 // good ol treasure coords class
+
+import java.util.ArrayList;
+
 public class TreasureCoordinates
 {
+    /*
+     * input for this will be a string with a digit in parentheses
+     * output will be arraylist of strings with coordinates in parentheses
+     * we will trust that the input will have at least 2 digits
+     */
+    public ArrayList determineCoordinates(String digits)
+    {
+        // for returning later
+        ArrayList<String> outCoords = new ArrayList<String>();
 
+        // get the parentheses out, dont need em
+        digits = digits.substring(1, digits.length() - 1);
+
+        return outCoords;
+    }
 }
