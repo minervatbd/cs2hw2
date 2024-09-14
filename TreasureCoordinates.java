6b3cf3cb0a982d0c3d7e71d474259e66f6e1cb2b
Minerva <minervatbd>
2024-09-14 15:23:27 -0400

    helper functions, first draft

--- TreasureCoordinates.java
+++ TreasureCoordinates.java
@@ -15,14 +15,59 @@ public class TreasureCoordinates
      * output will be arraylist of strings with coordinates in parentheses
      * we will trust that the input will have at least 2 digits
      */
-    public ArrayList determineCoordinates(String digits)
+    public ArrayList<String> determineCoordinates(String digits)
     {
         // for returning later
-        ArrayList<String> outCoords = new ArrayList<String>();
+        ArrayList<String> coords = new ArrayList<String>();
 
         // get the parentheses out, dont need em
         digits = digits.substring(1, digits.length() - 1);
 
-        return outCoords;
+        return coords;
+    }
+
+
+
+    // checks if coordinate string is valid
+    static boolean isCoordValid(String input)
+    {
+        // TODO
+        return true;
+    }
+
+    // prep strings for output
+    static String addParentheses(String input)
+    {
+        return "(" + input + ")";
+    }
+
+    // remove ', '
+    static String removeStr(String input, String target)
+    {
+        return input.replaceAll(target, "");
+    }
+
+    // remove .
+    static String removeChar(String input, char target)
+    {
+        return input.replace(target, (char)0);
+    }
+
+    // remove specific point
+    static String removePt(String input, boolean firstHalf)
+    {
+        String[] inSplit = input.split(", ");
+
+        if (firstHalf)
+            return removeChar(inSplit[0], '.') + ", " + inSplit[1];
+    
+        else
+            return inSplit[0] + ", " + removeChar(inSplit[1], '.');
+    }
+
+    // insert . or ,
+    static String insertStr(String input, String newStr, int pos)
+    {
+        return input.substring(0, pos) + newStr + input.substring(pos, input.length());
     }
 }
